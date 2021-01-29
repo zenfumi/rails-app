5cdbf5954aa846b10dad620697cca14ddf4b1d9d
zenfumi <ys00878@gmail.com>
2021-01-29 21:41:50 +0900

    submit Q1

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,9 @@
+class HomesController < ApplicationController
+  def index
+    @greet = "Hello World!!"
+    ##Userモデルをインスタンス化
+    user= User.new
+    #introduceメソッドを実行
+    @my_introduce = user.introduce
+  end
+end
